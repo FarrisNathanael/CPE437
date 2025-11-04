7f9052f7a67d7c03be495fe4f6a5602bdb2acfb0
StarlessWolf <71533101+StarlessWolf@users.noreply.github.com>
2025-11-03 23:40:26 -0800

    Added a javascript file and updated dark-mode to index and create page

---
+++ packages/proto/functions.js
@@ -0,0 +1,9 @@
+function toggleDarkMode(target, checked) {
+    const customEvent = new CustomEvent(
+        "dark-mode:toggle", {
+            bubbles: true,
+            detail: { checked },
+        });
+
+    target.dispatchEvent(customEvent);
+}
